e2b0555c0d1b9f5a2ed230b48e3d2bb50678b61b
Samreaksa Ros <15318223+monkeyidesign@users.noreply.github.com>
2019-05-20 11:27:39 +0700

    Adding extensions for managing UIView

--- Basic StackView/ViewController.swift
+++ Basic StackView/ViewController.swift
@@ -25,9 +25,19 @@ class ViewController: UIViewController {
         
         let stackView = UIStackView(arrangedSubviews: [redView, blueView])
         stackView.distribution = .fillEqually
+        stackView.axis = .vertical
         
         view.addSubview(stackView)
         stackView.frame = .init(x: 0, y: 0, width: 320, height: 250)
+        
+        //Enables auto layout for us
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        
     }
 
 
