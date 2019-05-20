b051351e7828db96e4eba91b683dbb5925b1794d
Samreaksa Ros <15318223+monkeyidesign@users.noreply.github.com>
2019-05-20 11:06:06 +0700

    Getting started with Stack View.

--- Basic StackView/ViewController.swift
+++ Basic StackView/ViewController.swift
@@ -4,7 +4,10 @@
 //
 //  Created by Apple on 5/20/19.
 //  Copyright © 2019 Monkey iDesign. All rights reserved.
-//
+//  Samreaksa Ros
+//  https://www.monkeyidesign.com
+//  email: sam@monkeyidesign.com
+//  tel: +85592 200069
 
 import UIKit
 
@@ -13,6 +16,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 320, height: 250)
     }
 
 
